de23b098bb0b27c9baad8c72757c903f7f1c620b
madisonmincevich <maddiebooldridge@gmail.com>
2022-03-22 19:42:48 -0800

    created search form

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import "./App.css";
 import icon from "./icon.png";
+import Dictionary from "./Dictionary.js";
 
 function App() {
   return (
     <div className="App">
-      <img src={icon} alt="" className="icon" />
-      <h1>Dictionary App</h1>
+      <div className="dictionaryApp">
+        <div className="container">
+          <img src={icon} alt="" className="icon" />
+          <h1>Dictionary App</h1>
+          <Dictionary />
+          <footer className="mt-4">Coded by Maddie Mincevich</footer>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/Dictionary.js
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+import "./Dictionary.css";
+
+export default function Dictionary() {
+  let [keyword, setKeyword] = useState("");
+
+  function search(event) {
+    event.preventDefault();
+    alert(`Searching for ${keyword} definition`);
+  }
+
+  function handleKeywordChange(event) {
+    setKeyword(event.target.value);
+  }
+
+  return (
+    <div className="Search">
+      <form onSubmit={search}>
+        <input
+          type="search"
+          placeholder="Search for a word..."
+          onChange={handleKeywordChange}
+        />
+      </form>
+    </div>
+  );
+}
