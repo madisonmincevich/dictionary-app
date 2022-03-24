06ad3671033c6bcb666d9ddfe8b9cc70ba65d87d
madisonmincevich <maddiebooldridge@gmail.com>
2022-03-24 15:16:00 -0800

    added synonyms

---
+++ src/Synonyms.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+export default function Synonyms(props) {
+  console.log(props.synonyms);
+  if (props.synonyms) {
+    return (
+      <ul className="Synonyms">
+        {props.synonyms.map(function (synonym, index) {
+          return (
+            <div>
+              <li key={index}>{synonym}</li>
+            </div>
+          );
+        })}
+      </ul>
+    );
+  } else {
+    return null;
+  }
+}
